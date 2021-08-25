ae7397fa7eee9d8f81446d557e05d8315a5ac108
dhashr <kalidashramachandran@gmail.com>
2021-08-25 10:47:37 +0530

    Commit Message

--- src/app.js
+++ src/app.js
@@ -26,9 +26,12 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
-
+        this.id = id,
+        this.name = name,
+        this.strength = this.getRandomStrength(),
+        this.image = "images/super-"+ (id + 1) +".png", 
+        this.type = type
     }
-
     // getting random strength
     getRandomStrength = () => {
         return Math.ceil(Math.random() * 100);
@@ -38,9 +41,22 @@ class Player {
     view = () => {
         // Accumulate HTML template
         // Type your code here
-
+        let player = document.createElement("div");
+        player.classList.add("player");
+        player.setAttribute("data-id", this.id);
+        if(this.selected == true){
+            player.classList.add("selected");
+        }
+        let image = document.createElement("img")
+        image.setAttribute("src",this.image)
+        let name = document.createElement("div")
+        name.textContent = this.name;
+        let strength = document.createElement("div")
+        strength.textContent = this.strength;
+        strength.className = "strength";
+        player.append(image, name, strength)
     
-        return player;
+     return player;
     }
 }
 
@@ -50,6 +66,11 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
+        this.players = players.map((player,i) =>{
+            let type = (i % 2 == 0) ? "hero" : "villan";
+            return new Player(i, player, type)
+
+        });
 
     }
 
